9f60727a3b430cc546c63e0a9e7b5ff331f53512
georgeshanti <georgeshanti@gmail.com>
2018-06-17 23:44:01 +0530

    Slide scroll is smooth. Added navbar and anchor links. All carousels working.

--- src/js/index.js
+++ src/js/index.js
@@ -9,8 +9,32 @@ import { HtoVScroll } from './horizontal-scroll'
     });
 
     window.addEventListener('wheel', e => {
-        hvscroll.onScroll(e.deltaY)
+        var windowWidth = window.innerWidth
+        var sign = ( e.deltaY / Math.abs(e.deltaY) )
+        hvscroll.onScroll(windowWidth * sign)
     },false)
+
+    document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+        anchor.addEventListener('click', function (e) {
+            e.preventDefault();
+            document.querySelector(this.getAttribute('href')).scrollIntoView({
+                behavior: 'smooth'
+            });
+        });
+    });
+    
+    
+
+
+    /* flickety testimonial*/
+    var flkty = new Flickity( '.main-gallery', {
+        cellAlign: 'left',
+        contain: true,
+        wrapAround: true,
+        prevNextButtons: false,
+        autoPlay: 1500,
+        prevNextButtons: true
+    });
 
     // drag
     // document.getElementsByClassName('slide-container')[0].addEventListener('mousedown',e => hvscroll.onMouseDown(e),false);
